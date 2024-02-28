fbbbe65f31bbdf3ba34517299b0468b633211d8e
miguelitodev <miguelrisquelme@gmail.com>
2024-02-27 22:17:07 -0300

    adding tailwind-variants to project

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,10 @@ export function App() {
 	return (
 		<div className="h-screen w-screen flex justify-center items-center flex-col bg-slate-900">
 			<Button kind="success">Entrar</Button>
-			<Button kind="danger">Sair</Button>
-			<Button className="bg-yellow-200">Oi</Button>
+			<Button size="sm" kind="danger">
+				Sair
+			</Button>
+			<Button size="xs">Oi</Button>
 		</div>
 	);
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,34 @@
 import { ComponentProps } from "react";
 import { Activity } from "lucide-react";
-import { twMerge } from "tailwind-merge";
+import { tv, VariantProps } from "tailwind-variants";
 
-export type ButtonProps = ComponentProps<"button"> & {
-	kind?: string;
-};
+const button = tv({
+	base: "flex justify-center items-center mt-3 bg-slate-50 text-slate-950 px-5 py-3 rounded font-medium data-[kind='success']:bg-emerald-300 data-[kind='danger']:bg-red-300",
+	variants: {
+		size: {
+			default: "h-10 px-4",
+			sm: "h-8 px-3",
+			xs: "h-6 px-2 text-xs",
+		},
+	},
+	defaultVariants: {
+		size: "default",
+	},
+});
 
-export function Button({ kind = "default", className, ...props }: ButtonProps) {
+export type ButtonProps = ComponentProps<"button"> &
+	VariantProps<typeof button> & {
+		kind?: string;
+	};
+
+export function Button({
+	kind = "default",
+	size,
+	className,
+	...props
+}: ButtonProps) {
 	return (
-		<button
-			data-kind={kind}
-			className={twMerge(
-				"flex justify-center items-center mt-3 bg-slate-50 text-slate-950 px-5 py-3 rounded font-medium data-[kind='success']:bg-emerald-300 data-[kind='danger']:bg-red-300 ",
-				className
-			)}
-			{...props}
-		>
+		<button data-kind={kind} className={button({ size, className })} {...props}>
 			{kind !== "default" ? <Activity className="h-4 w-4" /> : props.children}
 		</button>
 	);
